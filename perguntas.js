4794c233cc0aac45506e2e47741b5b902881d6cc
FNandin <luiz.ferreira.fernando@escola.pr.gov.br>
2025-09-05 14:19:58 +0000

    a

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é arquivo html?',
+    'O html e um arquivo de texto'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da França é moscow'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem nasceu em 5 de fevereiro de 1992?',
+    ' Neymar da silva santos junior'
+)
+
+criaCartao(
+    'Filosofia',
+    'Qual filosofo disse a frase "Penso, logo existo"?',
+    'Rene Descartes'
+)
